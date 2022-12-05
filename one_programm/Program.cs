6ccbd7c8738c6e9ce1d9b56d178bd19e14baee8b
Yulia Miller <miller82.98@mail.ru>
2022-12-04 22:42:03 -0800

    Добавьте файлы проекта.

---
+++ one_programm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        string MyName;
+        MyName = "Julia";
+        Console.WriteLine(MyName);
+        Console.ReadKey();
+    }
+}
